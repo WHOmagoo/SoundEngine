f03ae07d4375006c4cedfa193972089b087df0db
hmcgough <hrmagoo@gmail.com>
2020-02-23 02:36:54 -0800

    add clip class, represents a single loaded WAVE file

---
+++ src/sound/Clip.java
@@ -0,0 +1,114 @@
+package sound;
+
+import sound.frame.*;
+import sound.loader.AudioData;
+
+import java.util.ArrayList;
+
+public class Clip {
+    private SubBeatFrame[] subBeatFrames;
+    private int subBeatsCount;
+    private int soundFrameCount;
+
+    private Clip(SubBeatFrame[] subBeatFrames, int subBeatFrameSize, int subBeatsCount){
+        this.subBeatFrames = subBeatFrames;
+    }
+
+    public int getSize(){
+        return subBeatFrames.length;
+    }
+
+    public int getSoundFrameCount(){
+        return soundFrameCount;
+    }
+
+    public Clip(AudioData audioData, TimeSignature signature){
+        SoundFileFrame[] fileFrames = new SoundFileFrame[audioData.getData().length / audioData.getFormat().getFrameSize()];
+        for(int i = 0; i < fileFrames.length; i++){
+            fileFrames[i] = FrameFactory.makeSoundFrame(audioData.getData(), i, audioData.getFormat());
+            if(fileFrames[i] == null){
+                System.out.println(i + " was null");
+            }
+        }
+
+        System.out.printf("Data is %d bytes long. Clip size is %d frames\n", audioData.getData().length, fileFrames.length);
+
+        soundFrameCount = fileFrames.length;
+
+        subBeatFrames = new SubBeatFrame[signature.getSubBeatsCount(fileFrames.length)];
+
+        int prevEnd = 0;
+
+        for(int i = 0; i < subBeatFrames.length; i++){
+            int frameSize = signature.getFramesCountForFrameNumber(i);
+            int frameStart = signature.getFramesPreceeding(i);
+            SoundFileFrame[] soundFileFrames = new SoundFileFrame[frameSize];
+            System.arraycopy(fileFrames, frameStart, soundFileFrames, 0, soundFileFrames.length);
+            subBeatFrames[i] = new SubBeatFrame(soundFileFrames);
+
+            if(frameStart != prevEnd){
+                System.out.println("Skipping bytes");
+            }
+
+            prevEnd = frameStart + frameSize;
+        }
+    }
+
+
+    public SubBeatFrame getFrame(int frameNumber){
+        if(frameNumber < subBeatFrames.length){
+            return subBeatFrames[frameNumber];
+        } else {
+            return null;
+        }
+    }
+
+
+//    public byte[] toBytes(){
+//        byte[] result = new byte[subBeatFrameSize * subBeatsCount * 4];
+//        for(int i = 0; i < subBeatFrames.length; i++){
+//
+//            for(int j = 0; j < subBeatFrames[i].getFrames().length; j++) {
+//                byte[] frameBytes = subBeatFrames[i].getFrames()[j].getBytes();
+//                for(int copyIndex = 0; copyIndex < frameBytes.length; copyIndex++) {
+//                    result[i * subBeatFrameSize + j + copyIndex] = frameBytes[copyIndex];
+//                }
+//            }
+//        }
+//
+//        return result;
+//    }
+
+    public static Clip clipsCombiner(ArrayList<Clip> clips, int subBeatsCount, int subBeatFrameSize){
+
+        SubBeatFrame beatFrames[] = new SubBeatFrame[subBeatsCount];
+
+        for(int i = 0; i < beatFrames.length; i++){
+
+            SoundFileFrame soundFrames[] = new DefaultFrame[subBeatFrameSize];
+
+            for(int j = 0; j < soundFrames.length; j++){
+                soundFrames[j] = new DefaultFrame(0,0);
+            }
+
+            beatFrames[i] = new SubBeatFrame(soundFrames);
+        }
+
+        for(int i = 0; i < subBeatsCount && !clips.isEmpty(); i++){
+            ArrayList<Clip> toRemove = new ArrayList<>();
+            for(Clip c : clips){
+                for(int subBeat = 0; subBeat < subBeatsCount; subBeat++){
+                    if(subBeat > c.subBeatFrames.length){
+                        toRemove.add(c);
+                        break;
+                    }
+                    beatFrames[i].getFrames()[subBeat].add(c.subBeatFrames[i].getFrames()[subBeat]);
+                }
+            }
+
+            clips.removeAll(toRemove);
+        }
+
+        return new Clip(beatFrames, subBeatsCount, subBeatFrameSize);
+    }
+}
